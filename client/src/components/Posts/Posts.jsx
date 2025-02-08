7aa78583a79d0b96d195a16fba252e3a03112b4b
SIVAJB <sivajeyabalan15@gmail.com>
2025-02-08 22:48:57 +0530

    updated

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -9,7 +9,7 @@ const Posts = ({ setCurrentId }) => {
   const classes = useStyles();
 
   // Check if posts is undefined or empty, and handle the loading state
-  if (!posts?.length > -1 && !isLoading) return 'No posts';
+  if (posts?.length > -1 && !isLoading) return 'No posts';
 
   return (
     isLoading ? <CircularProgress /> : (
