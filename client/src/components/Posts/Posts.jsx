0a3108e60ae9991484786681b4bf0e785e12ef8c
SIVAJB <sivajeyabalan15@gmail.com>
2025-02-08 22:38:50 +0530

    changed posts

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -8,25 +8,27 @@ const Posts = ({ setCurrentId }) => {
   const { posts, isLoading } = useSelector((state) => state.posts);
   const classes = useStyles();
 
-  if (!posts.length && !isLoading) return 'No posts';
+  // Check if posts is undefined or empty, and handle the loading state
+  if (!posts || posts.length === 0) {
+    if (isLoading) return <CircularProgress />;
+    return 'No posts';
+  }
 
-  return (isLoading ?
-    <CircularProgress />
-    : (
-      <Grid
-        className={classes.container}
-        container
-        alignItems={"stretch"}
-        spacing={2}
-      >
-        {posts.map((post) => (
-          <Grid key={post._id} item xs={12} sm={12} md={6} lg={4}>
-            <Post post={post} setCurrentId={setCurrentId} />
-          </Grid>
-        ))}
-      </Grid>
-    )
+  return (
+    <Grid
+      className={classes.container}
+      container
+      alignItems={"stretch"}
+      spacing={2}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={12} md={6} lg={4}>
+          <Post post={post} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
+
 
 export default Posts;
