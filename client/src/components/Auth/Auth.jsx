bb161fb94dc391493807f6bfa203371f9082ef25
SIVAJB <sivajeyabalan15@gmail.com>
2025-02-13 23:56:21 +0530

    updated

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,42 +1,4 @@
-// Input.js
-import React from 'react';
-import { TextField, Grid, InputAdornment, IconButton } from '@mui/material';
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
-const Input = ({ name, half, handleChange, autoFocus, type, label, handleShowPassword, value }) => {
-    return (
-        <Grid item xs={12} sm={half ? 6 : 12}>
-            <TextField
-                name={name}
-                onChange={handleChange}
-                variant="outlined"
-                required
-                fullWidth
-                label={label}
-                autoFocus={autoFocus}
-                type={type}
-                value={value || ''}
-                InputProps={name === 'password' || name === 'confirmPassword' ? {
-                    endAdornment: (
-                        <InputAdornment position='end'>
-                            <IconButton
-                                onClick={handleShowPassword}
-                                aria-label={type === "password" ? "show password" : "hide password"}
-                            >
-                                {type === "password" ? <Visibility /> : <VisibilityOff />}
-                            </IconButton>
-                        </InputAdornment>
-                    )
-                } : null}
-            />
-        </Grid>
-    );
-};
-
-export default Input;
-
-// Auth.js
 import React, { useState, useEffect } from 'react';
 import { Avatar, Button, Paper, Grid, Typography, Container, Box, Alert } from '@mui/material';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
