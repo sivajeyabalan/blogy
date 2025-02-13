37987ca3ee689a8619efd80a94cd27c7a3da3964
SIVAJB <sivajeyabalan15@gmail.com>
2025-02-14 00:20:27 +0530

    updated

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const userSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String },
+  googleId: { type: String },
   id: { type: String },
 });
 
