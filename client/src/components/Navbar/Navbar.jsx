8489adaf2f8a8051c4ab3f607414ed7214a45740
SIVAJB <sivajeyabalan15@gmail.com>
2025-02-15 01:09:42 +0530

    updated

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -32,7 +32,6 @@ const Navbar = () => {
         }
         setUser(JSON.parse(localStorage.getItem("profile")));
     }, [location]);
-    console.log(user);
 
     const getAvatarContent = (user) => {
         if (!user?.result) return null;
